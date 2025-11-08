9f6482c2a1639f39f10250d1bf0b2ae7e78535a7
Eizo Shimada <e3shimada@gmail.com>
2025-11-08 15:43:54 +0900

    Create dummy.js

---
+++ rogue/func/system/dummy.js
@@ -0,0 +1,12 @@
+/**
+ * GitHub Pages
+ * が更新されない対策
+ * /rouge/func/system/ フォルダが移行されず
+ * wizard.jsがないエラーで起動しない為、
+ * 等ダミーファイルでsystemフォルダが作成されるか
+ * 試し
+ */
+function dummy_git_hut_pages_reflash_error(){
+
+        let a = 1;
+}
